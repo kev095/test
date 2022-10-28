ec5baf5a4ab375b74e130f1dd8c16ff0b4a4ff9c
kev095 <kevramos95@icloud.com>
2022-10-28 05:27:43 -0500

    refactor: deprecate old app

--- app.ts
+++ app.ts
@@ -1,97 +1,53 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import CreditCardModel from './src/model/Card';
-import db from './src/config/mongo';
-import { encodeCreditCard } from './src/utils/encodeCreditCard';
-import { validateCreditCard, yupValidationError } from './src/utils/validateCreditCard';
-import { DecodeResult } from './src/interfaces/decodeResult';
-import { decodeCreditCard } from './src/utils/decodeCreditCard';
-import { checkExpirationStatus } from './src/utils/checkExpirationStatus';
+import { MongoDbConfig } from './src/config/db';
+import { MongoDbService } from './src/service/db/MongoDbService';
+import { CardRepository } from './src/repository/card/CardRepository';
+import { TokenizationService } from './src/service/card/TokenizationService';
+import { TokenValidationService } from './src/service/token/TokenValidationService';
+import { TokenValidationHandler } from './src/handler/TokenValidationHandler';
+import { TokenCreationHandler } from './src/handler/TokenCreationHandler';
 
-db().then(() => console.log('Conexion Ready'));
+const dbConfig: MongoDbConfig = new MongoDbConfig();
+const dbService: MongoDbService = new MongoDbService(dbConfig);
 
-export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  let response: APIGatewayProxyResult;
+dbService.connect()
+  .then(() => {
+    console.log("[+] Database connected with Atlas Cluster")
+  })
+  .catch(err => {
 
-  try {
-    const token = event.headers.Authorization as string;
-    const PK = event.headers.pk;
-    if (!PK) {
-      response = {
-        statusCode: 500,
-        body: JSON.stringify({
-          message: 'pk is invalid'
-        })
-      };
-      return response;
+    if (err instanceof Error) {
+      console.table(
+        [
+          "name",
+          "message",
+          "stackTrace"
+        ],
+        [
+          err.name,
+          err.message,
+          err.stack as string,
+        ]
+      );
+
+      return;
     }
 
-    const decode: DecodeResult = decodeCreditCard(token);
-    if (decode.type === 'invalid-token') {
-      response = {
-        statusCode: 500,
-        body: JSON.stringify({ error: 'invalid token' })
-      };
-    }
-    const expiration = checkExpirationStatus(decode.sessioncreditCard);
-    if (expiration === 'expired') {
-      response = {
-        statusCode: 500,
-        body: JSON.stringify({ mess: 'token has expired' })
-      };
-    } else if (expiration === 'active') {
-      const { email, card_number, expiration_year, expiration_month } = decode.sessioncreditCard;
-      response = {
-        statusCode: 200,
-        body: JSON.stringify({ email, card_number, expiration_year, expiration_month })
-      };
-    }
-  } catch (err: unknown) {
     console.log(err);
-    response = {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: err instanceof Error ? err.message : 'some error happened --'
-      })
-    };
-  }
+  });
 
-  return response;
-};
+const tokenValidationService: TokenValidationService = new TokenValidationService();
+const tokenValidationHandler: TokenValidationHandler = new TokenValidationHandler(
+  tokenValidationService
+);
 
-export const tokenHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  let response: APIGatewayProxyResult;
-  try {
-    const reqBody = JSON.parse(event.body as string);
+export const lambdaHandler = tokenValidationHandler.process;
 
-    await validateCreditCard.validate(reqBody);
+const cardRepository: CardRepository = new CardRepository();
+const tokenizationService: TokenizationService = new TokenizationService(
+  cardRepository
+);
+const tokenCreationHandler: TokenCreationHandler = new TokenCreationHandler(
+  tokenizationService
+);
 
-    const creditCard = {
-      ...reqBody
-    };
-
-    const token = encodeCreditCard(creditCard);
-    const newCreditCard = new CreditCardModel({ ...creditCard, token: token.token });
-    const savedCreditCard = await newCreditCard.save();
-    if (savedCreditCard) {
-      response = {
-        statusCode: 200,
-        body: JSON.stringify(token)
-      };
-    } else {
-      response = {
-        statusCode: 500,
-        body: JSON.stringify('errror')
-      };
-    }
-  } catch (err: unknown) {
-    console.log(err);
-    response = {
-      statusCode: 500,
-      body: JSON.stringify({
-        error: err instanceof yupValidationError ? err.errors : 'some error   happened'
-      })
-    };
-  }
-
-  return response;
-};
+export const tokenHandler = tokenCreationHandler.process;
