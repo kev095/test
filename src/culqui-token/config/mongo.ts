3239312747bb3c8192eff32773a93bc6b70075e1
kev095 <kevramos95@icloud.com>
2022-10-28 01:08:50 -0500

    config(env): use MONGODB_URL variable in order to secret in application

--- src/culqui-token/config/mongo.ts
+++ src/culqui-token/config/mongo.ts
@@ -1,9 +1,7 @@
 import { connect } from 'mongoose';
 
 async function dbConnect(): Promise<void> {
-    connect(
-        'mongodb://vekjs:p131471483@cluster0-shard-00-00.yzzb4.mongodb.net:27017,cluster0-shard-00-01.yzzb4.mongodb.net:27017,cluster0-shard-00-02.yzzb4.mongodb.net:27017/db_next?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
-    );
+    connect(String(process.env.MONGODB_URL));
 }
 
 export default dbConnect;
